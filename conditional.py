be35bccb3418267dbb2b356bde580ee8b941fc73
rafaela_mansini <rafaela_mansini@hotmail.com>
2020-04-22 19:23:16 -0300

    Lesson: for create and change print of for

--- conditional.py
+++ conditional.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
+
+#Lesson: while structure
 x = 1
 y = 5
 
 def compare_values(x, y):
 
     if x > y:
-        print("x is higher than y")
+        #print("x is higher than y")
+        return "x is higher than y"
     else:
         if x == y:
-            print ("x is the same than y")
+            #print ("x is the same than y")
+            return "x is the same than y"
         else:
-            print("x is less than y")
+            #print("x is less than y")
+            return "x is less than y"
 
 while x < 10:
-    print(x)
+    print x,": ",compare_values(x, y)
     compare_values(x, y)
     x += 1
+
+print("\n")
+#Lesson: for structure
+list_of_king_books = ["It", "The Stand", "The Shining", "The Institute", "The Outsider", "Doctor Sleep", "November 63", "Joyland"]
+
+for book in list_of_king_books:
+    print(book)
