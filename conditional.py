434250041f1ca359b30d3342885ad56c7ced21c9
rafaela_mansini <rafaela_mansini@hotmail.com>
2020-04-22 18:59:41 -0300

    Inserting a function to compare values

--- conditional.py
+++ conditional.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 x = 1
-y = 10
-if x > y:
-    print("x e maior que y")
-else:
-    if x == y:
-        print ("x é igual a y")
+y = 5
+
+def compare_values(x, y):
+
+    if x > y:
+        print("x is higher than y")
     else:
-        print("y e maior que x")
+        if x == y:
+            print ("x is the same than y")
+        else:
+            print("x is less than y")
+
 while x < 10:
     print(x)
+    compare_values(x, y)
     x += 1
