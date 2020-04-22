2c4c936c44d692491e4d275db4a536883071776c
rafaela_mansini <rafaela_mansini@hotmail.com>
2020-04-22 18:17:49 -0300

    First class of conditional structures

---
+++ conditional.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+x = 1
+y = 10
+if x > y:
+    print("x e maior que y")
+else:
+    if x == y:
+        print ("x é igual a y")
+    else:
+        print("y e maior que x")
+while x < 10
+    print(x)
+    x += 1
